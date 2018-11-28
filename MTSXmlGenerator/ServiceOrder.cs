9e81f23f3e91a28f8569d2793d0dd9f2907a4625
ichavera <chaveraivan@yahoo.com>
2018-11-28 15:37:23 -0600

    Made a Service Order class to move sevice order related functions out of Main.

---
+++ MTSXmlGenerator/ServiceOrder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MtsXmlGenerator
+{
+    class ServiceOrder
+    {
+        public static void setUpServiceOrder(string nmi, string checksum)
+        {
+            Random random = new Random();
+            int rng = random.Next(100, 1000);
+
+            string doc = File.ReadAllText(@"..\..\Templates\SO_NC_Itron_Template.xml");
+
+            doc = doc.Replace("@NMI_TEMP", nmi);
+            doc = doc.Replace("@CHECKSUM", checksum);
+            doc = doc.Replace("@RNG_SO", nmi + checksum + rng);
+
+            File.WriteAllText(@"..\..\Templates\sord" + "_test_Temp" + nmi + rng + ".xml", doc);
+        }
+    }
+}
