dd4069672694aef946b14b3539a8ea632d579dd5
DerekSittenauer <sittenauerd@gmail.com>
2018-11-28 15:15:12 -0600

    Added to and from fields

--- MTSXmlGenerator/Payloads.cs
+++ MTSXmlGenerator/Payloads.cs
@@ -19,16 +19,16 @@ namespace MtsXmlGenerator
             string payload2 = File.ReadAllText(@"..\..\Payloads\Payload2_DIC.sql");
             string payload3 = File.ReadAllText(@"..\..\Payloads\Payload3_DIC.sql");
 
-            payload1 = modifyPayload(payload1, nmi, checksum, rng);
-            payload2 = modifyPayload(payload2, nmi, checksum, rng);
-            payload3 = modifyPayload(payload3, nmi, checksum, rng);
+            payload1 = modifyPayload(payload1, nmi, checksum, rng, "POWCP1", "POWERCR1");
+            payload2 = modifyPayload(payload2, nmi, checksum, rng, "POWCP1", "POWERCR1");
+            payload3 = modifyPayload(payload3, nmi, checksum, rng, "POWCP1", "POWERCR1");
 
             File.WriteAllText(@"..\..\Payloads\Payload1_test.sql", payload1);
             File.WriteAllText(@"..\..\Payloads\Payload2_test.sql", payload2);
             File.WriteAllText(@"..\..\Payloads\Payload3_test.sql", payload3);
         }
 
-        private static string modifyPayload(string payload, string nmi, string checksum, int rng)
+        private static string modifyPayload(string payload, string nmi, string checksum, int rng, string to, string from)
         {
             string meterID = checksum + nmi.Substring(0, 3) + rng;
 
@@ -36,6 +36,8 @@ namespace MtsXmlGenerator
             payload = payload.Replace("@METER_ID", meterID);
             payload = payload.Replace("@METER_NUMBER", "EQP" + meterID.Substring(0, 6));
             payload = payload.Replace("@CHANNEL", meterID);
+            payload = payload.Replace("@TO", to);
+            payload = payload.Replace("@FROM", from);
 
             return payload;
         }
