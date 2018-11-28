db6afeedcf9572717588571929284cb1844903ea
ratrest1 <Alex.Trest@Itron.com>
2018-11-28 15:05:29 -0600

    I make button click

---
+++ XmlGenGUI/Controllers/SOGenController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XmlGenGUI.Controllers
+{
+    public class SOGenController : Controller
+    {
+        public ActionResult XMLGen()
+        {
+
+            ViewBag.Message = "Service Order XML Generation";
+
+            return View();
+        }
+
+        public ActionResult SOXmlGen()
+        {
+
+            ViewBag.Message = "Service Order XML Generation";
+
+            return View();
+        }
+
+        
+    }
+}
---
+++ XmlGenGUI/Controllers/SOGenXmlPage.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace XmlGenGUI.Controllers
+{
+    public partial class SOGenXmlPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Debug.Write("Load!\n");
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            Debug.Write("\nClick!\n");
+        }
+    }
+}
