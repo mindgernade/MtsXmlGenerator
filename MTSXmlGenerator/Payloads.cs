1d95dc84dd9550dffd948a5f85b5b345d3802e38
DerekSittenauer <sittenauerd@gmail.com>
2018-11-29 09:57:33 -0600

    Changed strings to StringBuilder

--- MTSXmlGenerator/Payloads.cs
+++ MTSXmlGenerator/Payloads.cs
@@ -15,20 +15,20 @@ namespace MtsXmlGenerator
             Random random = new Random();
             int rng = random.Next(100, 999);
 
-            string payload1 = File.ReadAllText(@"..\..\Payloads\Payload1_DIC.sql");
-            string payload2 = File.ReadAllText(@"..\..\Payloads\Payload2_DIC.sql");
-            string payload3 = File.ReadAllText(@"..\..\Payloads\Payload3_DIC.sql");
+            StringBuilder payload1 = new StringBuilder(File.ReadAllText(@"..\..\Payloads\Payload1_DIC.sql"));
+            StringBuilder payload2 = new StringBuilder(File.ReadAllText(@"..\..\Payloads\Payload2_DIC.sql"));
+            StringBuilder payload3 = new StringBuilder(File.ReadAllText(@"..\..\Payloads\Payload3_DIC.sql"));
 
             payload1 = modifyPayload(payload1, nmi, checksum, rng, to, from);
             payload2 = modifyPayload(payload2, nmi, checksum, rng, to, from);
             payload3 = modifyPayload(payload3, nmi, checksum, rng, to, from);
 
-            File.WriteAllText(@"..\..\Payloads\Payload1_test.sql", payload1);
-            File.WriteAllText(@"..\..\Payloads\Payload2_test.sql", payload2);
-            File.WriteAllText(@"..\..\Payloads\Payload3_test.sql", payload3);
+            File.WriteAllText(@"..\..\Payloads\Payload1_test.sql", payload1.ToString());
+            File.WriteAllText(@"..\..\Payloads\Payload2_test.sql", payload2.ToString());
+            File.WriteAllText(@"..\..\Payloads\Payload3_test.sql", payload3.ToString());
         }
 
-        private static string modifyPayload(string payload, string nmi, string checksum, int rng, string to, string from)
+        private static StringBuilder modifyPayload(StringBuilder payload, string nmi, string checksum, int rng, string to, string from)
         {
             string meterID = checksum + nmi.Substring(0, 3) + rng;
 
